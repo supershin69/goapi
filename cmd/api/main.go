b4fa2cee4d9aff08c97356f07cb015b215f2ee92
supershin69 <mgshinthant58@gmail.com>
2025-05-20 06:48:18 +0630

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/supershin69/goapi/internal/handlers"
+	log "github.com/sirupsen/logrus"
+)
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/supershin69/goapi
+
+go 1.24.3
