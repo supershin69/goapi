f4d7461d0c87cd8ee0e4cf3c10e6a153be3e4ffd
supershin69 <mgshinthant58@gmail.com>
2025-05-20 07:04:01 +0630

    Some changes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,3 +9,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func main() {
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GO API service....")
+
+	fmt.Println(`C
+	0
+	0
+	L`)
+	err := http.ListenAndServe("localhost:8000", r)
+}
